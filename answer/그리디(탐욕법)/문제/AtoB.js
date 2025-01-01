6f958584ed542c655f4a1eda11c8203be083d5ac
ygvbhy <ygvbhy@naver.com>
2025-01-01 17:27:06 +0900

    A to B 추가 문제 풀이

--- answer/그리디(탐욕법)/문제/AtoB.js
+++ answer/그리디(탐욕법)/문제/AtoB.js
@@ -17,3 +17,22 @@ while (k >= n) {
 }
 
 console.log(n == k ? count + 1 : -1);
+
+// 추가 문제 풀이
+let [a, b] = input[0].split(" ").map(Number);
+let flag = false;
+let result = 1;
+
+while (a <= b) {
+  if (a == b) {
+    flag = true;
+    break;
+  }
+  if (b % 2 == 0) b = parseInt(b / 2);
+  else if (b % 10 == 1) b = parseInt(b / 10);
+  else break;
+  result++;
+}
+
+if (flag) console.log(result);
+else console.log(-1);
